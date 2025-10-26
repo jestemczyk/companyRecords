4e9a4b3e3c60be1b990100e5135260b7e3277a0d
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2025-10-26 18:48:42 +0200

    adding form updated

--- src/components/addEmployeeFormBlock/AddingForm.jsx
+++ src/components/addEmployeeFormBlock/AddingForm.jsx
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 export const AddingForm = ({ setEmplList }) => {
   const inputStyle =
     "w-2/6 h-10 mr-5 p-5 bg-white text-black focus:outline-none rounded-[5px]";
   const input1Ref = useRef(null);
   const input2Ref = useRef(null);
+  const [numInputValue, setNumInputValue] = useState("");
   function onSubmitFunction() {
     setEmplList((prev) => {
       const newId = prev[prev.length - 1].id + 1;
@@ -18,12 +19,21 @@ export const AddingForm = ({ setEmplList }) => {
         },
       ];
     });
+    input1Ref.current.value = "";
+    setNumInputValue("");
   }
+  const handleChangeNumInput = (e) => {
+    const input = e.target.value;
+    const toNumbers = input.replace(/\D/g, "");
+    setNumInputValue(toNumbers);
+  };
   return (
     <form
       onSubmit={(ev) => {
         ev.preventDefault();
-        onSubmitFunction();
+        if (input1Ref.current.value && input2Ref.current.value) {
+          onSubmitFunction();
+        }
       }}
     >
       <input
@@ -33,10 +43,11 @@ export const AddingForm = ({ setEmplList }) => {
         className={inputStyle}
       />
       <input
-        type="number"
         placeholder="Salary in $"
         ref={input2Ref}
         className={inputStyle}
+        value={numInputValue}
+        onChange={handleChangeNumInput}
       />
       <button className="ml-10 w-30 h-10 rounded-[5px] border border-white text-white bg-gray-700 hover:text-black hover:bg-white">
         Add
